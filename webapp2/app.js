739d471e040701eb52a5409a5f291d0965541ffd
marcokuang <marcojkuang@gmail.com>
2017-12-31 10:26:44 -0800

    added webapp3

--- webapp2/app.js
+++ webapp2/app.js
@@ -1,1 +1,23 @@
+/**
+ * We can use the process object to get environment information, 
+ * read environment variables, communicate with the terminal, or parent processes
+ */
+
 console.log(process.argv);
+
+
+function validate(keyword) {
+    let index = process.argv.indexOf(keyword);
+
+    return ((index === -1) ? null : process.argv[index + 1]);
+}
+
+let month = validate('--month');
+let date = validate('--date');
+
+if (month == null || date == null){
+    console.log("ERROR, PLEASE TRY AGAIN!");
+}
+else{
+    console.log(`The date you chose is ${month} ${date}`);
+}
---
+++ webapp3/ask.js
@@ -0,0 +1,1 @@
+process.stdout.write("Hello stdout\n\n\n");
